d3b4175580bf7dfe419fcb7fc19b9681c2f0a671
Marco <martinez.marco.1027@gmail.com>
2022-05-01 15:51:33 -0600

    Fix #6
    Fix issue #6 - now Circle2.intersects correctly computes when circles
    are intersecting or not

--- Circles/src/edu/nmsu/cs/circles/Circle2.java
+++ Circles/src/edu/nmsu/cs/circles/Circle2.java
@@ -10,13 +10,37 @@ public class Circle2 extends Circle
 
 	public boolean intersects(Circle other)
 	{
-		double d;
-		d = Math.sqrt(Math.pow(center.x - other.center.x, 2) +
-				Math.pow(center.y - other.center.y, 2));
-		if (d < radius)
+		// distance between the two centers
+		double dist = Math.sqrt((Math.pow(this.center.x - other.center.x, 2)) + (Math.pow(this.center.y - other.center.y, 2)));
+		// sum of the radii
+		double radSum = this.radius + other.radius;
+		
+		// this.circle is inside of the other circle, but they're touching at a point
+		// and vice-versa
+		if (dist + this.radius == other.radius) {
 			return true;
-		else
+		}
+		
+		if (dist + other.radius == this.radius) {
+			return true;
+		}
+		
+		// this.circle is inside of the other circle, but they're not intersecting
+		// and vice-versa
+		if (dist + this.radius < other.radius) {
 			return false;
+		}
+		
+		if (dist + other.radius < this.radius) {
+			return false;
+		}
+		
+		// normal case
+		if (dist <= radSum) {
+			return true;
+		}
+		
+		return false;
 	}
 
 }
--- Circles/src/edu/nmsu/cs/circles/Circle2Test.java
+++ Circles/src/edu/nmsu/cs/circles/Circle2Test.java
@@ -35,6 +35,141 @@ public class Circle2Test
 		System.out.println("\nTest finished.");
 	}
 
+	
+	//
+	// Tests that two circles that are not intersecting are not intersecting
+	//
+	@Test
+	public void intersectNoIntersection() {
+		
+		System.out.println("Running test: intersectNoIntersection.");
+		
+		// next to eachother same x
+		System.out.println("intersectNoIntersection - next to eachother");
+		Circle2 a = new Circle2(0, 50, 10);
+		Circle2 b = new Circle2(0, 0, 5);
+		
+		Assert.assertFalse(a.intersects(b));
+		Assert.assertFalse(b.intersects(a));
+		
+		// one above the other, almost touching
+		System.out.println("intersectNoIntersection - one above, almost touching");
+		a = new Circle2(1, 10, 2.99);
+		b = new Circle2(1, 5, 2);
+		
+		Assert.assertFalse(a.intersects(b));
+		Assert.assertFalse(b.intersects(a));
+		
+		// circle above to the right
+		System.out.println("intersectNoIntersection - above to the right, very close");
+		a = new Circle2(16, 128, 4);
+		b = new Circle2(23.45, 121.6, 5);
+		
+		Assert.assertFalse(a.intersects(b));
+		Assert.assertFalse(b.intersects(a));
+		
+		// circle is inside the other circle
+		System.out.println("intersectNoIntersection - circle a is inside circle b");
+		a = new Circle2(0, 0, 2);
+		b = new Circle2(0, 0, 5);
+		
+		// circle is inside the other circle
+		System.out.println("intersectNoIntersection - circle b is inside circle a");
+		a = new Circle2(0, 0, 5);
+		b = new Circle2(0, 0, 2);
+		
+		Assert.assertFalse(a.intersects(b));
+		Assert.assertFalse(b.intersects(a));
+		
+	} // end noIntersection
+	
+	//
+	// Tests two circles that are completly overlapping
+	//
+	@Test
+	public void intersectCompleteOverlap() {
+		
+		System.out.println("Running test: intersectCompleteOverlap.");
+		
+		// next to eachother same x
+		System.out.println("intersectCompleteOverlap - at origin");
+		Circle2 a = new Circle2(0, 0, 4);
+		Circle2 b = new Circle2(0, 0, 4);
+		
+		Assert.assertTrue(a.intersects(b));
+		Assert.assertTrue(b.intersects(a));
+	}
+	
+	//
+	// Tests two circles that are intersecting at a single point
+	//
+	@Test
+	public void intersectAtOnePoint() {
+		
+		System.out.println("Running test: intersectAtOnePoint.");
+		
+		// next to each other same x
+		System.out.println("intersectAtOnePoint - on x-axis");
+		Circle2 a = new Circle2(0, 0, 3);
+		Circle2 b = new Circle2(5, 0, 2);
+		
+		Assert.assertTrue(a.intersects(b));
+		Assert.assertTrue(b.intersects(a));
+	}
+	
+	//
+	// Tests two circles that are intersecting at two points
+	//
+	@Test
+	public void intersectAtTwoPoints() {
+		
+		System.out.println("Running test: intersectAtTwoPoints.");
+		
+		// next to eachother same x
+		System.out.println("intersectAtTwoPoints - on x-axis");
+		Circle2 a = new Circle2(2, 0, 1);
+		Circle2 b = new Circle2(4, 0, 2);
+		
+		Assert.assertTrue(a.intersects(b));
+		Assert.assertTrue(b.intersects(a));
+	}
+	
+	//
+	// Tests scaling a circle to a bigger circle
+	//
+	@Test
+	public void scaleBigger() {
+		
+		System.out.println("Running test: scaleBigger.");
+		
+		// next to eachother same x
+		System.out.println("scaleBigger - x 2.0");
+		
+		circle2 = new Circle2(1, 1, 1);
+		
+		circle2.scale(2.0);
+		
+		Assert.assertTrue(circle2.radius == 2.0);
+	}
+	
+	//
+	// Tests scaling a circle to a smaller circle
+	//
+	@Test
+	public void scaleSmaller() {
+		
+		System.out.println("Running test: scaleSmaller.");
+		
+		// next to eachother same x
+		System.out.println("scaleSmaller - x 0.5");
+		
+		circle2 = new Circle2(1, 1, 1);
+		
+		circle2.scale(0.5);
+		
+		Assert.assertTrue(circle2.radius == 0.5);
+	}
+	
 	//
 	// Test a simple positive move
 	//
@@ -64,5 +199,6 @@ public class Circle2Test
 	 * java.lang.Class.forName("Circle1Test")); } catch (Exception e) { System.out.println("Exception:
 	 * " + e); } }
 	 ***/
+
 
 }
